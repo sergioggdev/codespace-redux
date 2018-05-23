a1a44da4fb00fd5a1d7b12efbe9ed07e47be8d75
Sergio Garcia <s.b.garcia.garcia@accenture.com>
2018-05-24 00:16:01 +0200

    Create enviroment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Reveal from "reveal";
+import "reveal/index.css";
+import "reveal/theme/default.css";
+
+// Full config https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+  history: true,
+  transition: "slide"
+});
