97ad6d631f700171eba24755d5453c327268e24c
Sergio Garcia <s.b.garcia.garcia@accenture.com>
2018-05-24 22:11:43 +0200

    Progress masterClass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import Reveal from 'reveal';
+
 import 'reveal/index.css';
-import 'reveal/theme/beige.css';
+import 'reveal/theme/default.css';
+
+import welcome from './img/cover1.jpg';
+import theIssue from './img/domar-js.jpeg';
+import lego from './img/lego.jpg';
+import facebook from './img/facebook.jpg';
+import what from './img/what.gif';
+import redux from './img/redux-cover.png';
+
+document.querySelector('#welcome').setAttribute('data-background-image', welcome);
+document.querySelector('#issue-home').setAttribute('data-background-image', theIssue);
+document.querySelector('#issue-end').setAttribute('data-background-image', lego);
+document.querySelector('#flux-home').setAttribute('data-background-image', facebook);
+document.querySelector('#flux-what').setAttribute('data-background-image', what);
+document.querySelector('#redux-home').setAttribute('data-background-image', redux);
 
 Reveal.initialize({
   history: true,
