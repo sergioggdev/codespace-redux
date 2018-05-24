e4e66297ff07d296ca4af14e9e2817867f8c4e92
Sergio Garcia <sergio.garcia.seo@gmail.com>
2018-05-24 02:22:12 +0200

    Migracion presentacion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import Reveal from "reveal";
-import "reveal/index.css";
-import "reveal/theme/default.css";
+import Reveal from 'reveal';
+import 'reveal/index.css';
+import 'reveal/theme/beige.css';
 
-// Full config https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
   history: true,
-  transition: "slide"
 });
